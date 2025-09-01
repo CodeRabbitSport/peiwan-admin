643d38d87ba3f9b512778f9e47d73ae2bd54d7a9
soya <3100034521@qq.com>
2025-09-01 15:30:29 +0800

    修改依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,15 @@
-import {resolve} from 'path'
-import type {ConfigEnv, UserConfig} from 'vite'
-import {loadEnv} from 'vite'
-import {createVitePlugins} from './build/vite'
-import {exclude, include} from "./build/vite/optimize"
+import type { ConfigEnv, UserConfig } from 'vite'
+import { loadEnv } from 'vite'
+import { createVitePlugins } from './build/vite'
+import { exclude, include } from "./build/vite/optimize"
+import { fileURLToPath } from 'url'
 // 当前执行node命令时文件夹的地址(工作目录)
 const root = process.cwd()
 
-// 路径查找
-function pathResolve(dir: string) {
-    return resolve(root, '.', dir)
-}
+const dir = (dir: string = '.') => fileURLToPath(new URL(dir, import.meta.url))
 
 // https://vitejs.dev/config/
-export default ({command, mode}: ConfigEnv): UserConfig => {
+export default ({ command, mode }: ConfigEnv): UserConfig => {
     let env = {} as any
     const isBuild = command === 'build'
     if (!isBuild) {
@@ -44,23 +41,15 @@ export default ({command, mode}: ConfigEnv): UserConfig => {
             preprocessorOptions: {
                 scss: {
                     additionalData: '@use "@/styles/variables.scss" as *;',
-                    javascriptEnabled: true,
                     silenceDeprecations: ["legacy-js-api"], // 参考自 https://stackoverflow.com/questions/78997907/the-legacy-js-api-is-deprecated-and-will-be-removed-in-dart-sass-2-0-0
                 }
             }
         },
         resolve: {
             extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.scss', '.css'],
-            alias: [
-                {
-                    find: 'vue-i18n',
-                    replacement: 'vue-i18n/dist/vue-i18n.cjs.js'
-                },
-                {
-                    find: /\@\//,
-                    replacement: `${pathResolve('src')}/`
-                }
-            ]
+            alias: {
+                '@': dir('./src')
+            },
         },
         build: {
             minify: 'terser',
@@ -76,13 +65,13 @@ export default ({command, mode}: ConfigEnv): UserConfig => {
             rollupOptions: {
                 output: {
                     manualChunks: {
-                      echarts: ['echarts'], // 将 echarts 单独打包，参考 https://gitee.com/yudaocode/yudao-ui-admin-vue3/issues/IAB1SX 讨论
-                      'form-create': ['@form-create/element-ui'], // 参考 https://github.com/yudaocode/yudao-ui-admin-vue3/issues/148 讨论
-                      'form-designer': ['@form-create/designer'],
+                        echarts: ['echarts'], // 将 echarts 单独打包，参考 https://gitee.com/yudaocode/yudao-ui-admin-vue3/issues/IAB1SX 讨论
+                        'form-create': ['@form-create/element-ui'], // 参考 https://github.com/yudaocode/yudao-ui-admin-vue3/issues/148 讨论
+                        'form-designer': ['@form-create/designer'],
                     }
                 },
             },
         },
-        optimizeDeps: {include, exclude}
+        optimizeDeps: { include, exclude }
     }
 }
